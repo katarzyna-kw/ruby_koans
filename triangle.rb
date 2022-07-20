7108150b7c654447846c16546561d6604fddb25d
katarzyna-kw <katarzyna.koch@gmail.com>
2022-07-20 17:53:54 -0500

    154/281

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,16 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a == b && b == c && a == c
+    :equilateral
+  elsif (a == b && b != c) || (a == c && a != b) || (b == c && a != b)
+    :isosceles
+  elsif a != b && a != c && b != c
+    :scalene
+  else
+    nil
+  end
+
 end
 
 # Error class used in part 2.  No need to change this code.
